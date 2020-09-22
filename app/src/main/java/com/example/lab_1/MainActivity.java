6e5035b1f8f8068d580d5b729231c04788f87237
Cristian Morillas Escobar <cvme0001@red.ujaen.es>
2020-09-22 14:20:23 +0300

    Change text button added

--- app/src/main/java/com/example/lab_1/MainActivity.java
+++ app/src/main/java/com/example/lab_1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lab_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.widget.TextView;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void buttonChangeText(View view)
+    {
+        TextView tv = findViewById(R.id.textView1);
+        tv.setText("Yeaah :)");
+    }
+    /*
+    public void buttonChangeColor(View view)
+    {
+        TextView tv = (TextView)findViewById(R.id.textView1);
+        tv.setTextColor(Color.RED);
+    }
+    */
+
 }
